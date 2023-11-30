6c2b46d185bdfb3af74b0999df2f5e0179f4deac
lokesh kallepalli <142959946+lok25esh@users.noreply.github.com>
2023-11-29 20:31:48 -0600

    Updated index.js

--- routes/university.js
+++ routes/university.js
@@ -1,15 +1,22 @@
 var express = require('express');
-    const university_controlers= require('../controllers/university');
-    var router = express.Router();
+const university_controlers= require('../controllers/university');
+var router = express.Router();
+
+const secured = (req, res, next) => {
+    if (req.user) {
+        return next();
+    }
+    res.redirect("/login");
+}
     /* GET university */
-    router.get('/', university_controlers.university_view_all_Page );
+router.get('/', university_controlers.university_view_all_Page );
     /* GET detail university page */
 router.get('/detail', university_controlers.university_view_one_Page);
 /* GET create university page */
-router.get('/create', university_controlers.university_create_Page);
+router.get('/create', secured, university_controlers.university_create_Page);
     module.exports = router;
     /* GET create update page */
-router.get('/update', university_controlers.university_update_Page);
+router.get('/update', secured, university_controlers.university_update_Page);
 /* GET delete university page */
-router.get('/delete', university_controlers.university_delete_Page);
+router.get('/delete', secured, university_controlers.university_delete_Page);
 
