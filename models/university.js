ddb71ae382962641873d61a539fd96d14944c49d
lokesh kallepalli <142959946+lok25esh@users.noreply.github.com>
2023-11-29 20:28:13 -0600

    Updated university.js

--- models/university.js
+++ models/university.js
@@ -1,8 +1,36 @@
 const mongoose = require("mongoose")
 const universitySchema = mongoose.Schema({
-unvName: String,
-unvSid: Number,
-unvFee: Number
+
+unvName: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 20,
+    
+    validate: {
+        validator: function (v) {
+            return /^[a-zA-Z0-9_]+$/.test(v); 
+        },
+        message: props => `${props.value} is not a valid University Name! Use only letters, numbers, and underscores.`,
+    },
+},
+unvSid: {
+    type: Number,
+    validate: {
+        validator: function(v) {
+            return v >= 0; 
+        },
+        message: props => `${props.value} is not a valid id! ID must be a non-negative number.`,
+    },
+},
+unvFee: {
+    type: Number,
+    validate: {
+        validator: function(v) {
+            return v >= 0; 
+        },
+        message: props => `${props.value} is not a valid Fee! Fee must be a non-negative number.`,
+    },
+},
 })
-module.exports = mongoose.model("university",
-universitySchema)
+module.exports = mongoose.model("university", universitySchema)
